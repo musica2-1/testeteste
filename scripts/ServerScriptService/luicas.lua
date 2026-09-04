e8a91a33bb21ebc6caa87cc0c9ad8206b36099a0
musica2-1 <musica2-1@users.noreply.github.com>
2026-09-03 23:51:14 -0300

    save

--- scripts/ServerScriptService/luicas.lua
+++
@@ -1,9 +0,0 @@
-print("Hello world!")
-print("Hello world!")
-print("Hello world!")
-print("Hello world!")
-print("Hello world!")
-print("Hello world!")
-alksldlaksdlkasd
-print("Hello world!")
-print("Hello world!")
